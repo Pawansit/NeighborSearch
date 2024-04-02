cbed1eaf14950e00612977e77f99ed888a20d9f6
Pawan Kumar <pawanomar29@gmail.com>
2024-04-02 15:34:09 +0530

    First Program

--- ContactList.py
+++ ContactList.py
@@ -3,25 +3,34 @@ import biotite.structure.io.pdbx as pdbx
 import biotite.structure as Struc
 
 
-file = pdbx.PDBxFile.read("../../../PDBAPI/ISDA_Collections/ForTestingdataRecord/6lu7.cif")
-stru = pdbx.get_structure(file)[0]
 
-cell_list = Struc.CellList(stru, 5)
-
-ref_ligand = stru[stru.res_name == "010"]
-ref_ligand_center = Struc.centroid(ref_ligand)
-
-indices = cell_list.get_atoms(ref_ligand_center, radius=6.0)
-ContactList = stru[indices]
-
-ids, names = Struc.get_residues(ContactList)
-Chainids = Struc.get_chains(ContactList)
-
-R1 = Struc.centroid(ContactList[ContactList.res_name == '010'])
-
-for m1, m2, m3 in zip(ids, names, Chainids):
-    R2 = Struc.centroid(ContactList[ContactList.res_id == m1])
-    if m2 == "010": continue
-    print(m1, m2, m3,Struc.distance(R1, R2), sep = "\t")
+def NeighbourList(PDBFile, ligand_id, cutoff) :
 
 
+    file = pdbx.PDBxFile.read(PDBFile)
+    Structure = pdbx.get_structure(file)[0]
+
+    Cell_list = Struc.CellList(Structure, 10)
+
+    Ref_ligand = Structure[Structure.res_name == ligand_id]
+    Ref_ligand_center = Struc.centroid(Ref_ligand)
+
+    Indices = Cell_list.get_atoms(Ref_ligand_center, radius=cutoff)
+    ContactList = Structure[Indices]
+
+    Ids, Names = Struc.get_residues(ContactList)
+    Chainids = Struc.get_chains(ContactList)
+
+    R1 = Struc.centroid(ContactList[ContactList.res_name == ligand_id])
+
+    for m1, m2, m3 in zip(Ids, Names, Chainids):
+        R2 = Struc.centroid(ContactList[ContactList.res_id == m1])
+        if m2 == ligand_id: continue
+        print(m1, m2, m3,Struc.distance(R1, R2), sep = "\t")
+
+
+if __name__ == "__main__":
+    pdb_file_path = "../../../PDBAPI/ISDA_Collections/ForTestingdataRecord/6lu7.cif"
+    ligand_id = '010'
+    Cutoff = 6
+    NeighbourList(pdb_file_path, ligand_id, Cutoff)
